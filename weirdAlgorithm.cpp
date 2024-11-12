65f7d0f3474335832bdd70210619f564bee14601
Niraj singh <fgniraj@gmail.com>
2024-11-12 10:33:39 +0530

    Code added for weirdAlgorithms

--- weirdAlgorithm.cpp
+++ weirdAlgorithm.cpp
@@ -1,7 +1,18 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-cout<<"Hello world";
-return 0;
+ 
+int main() {
+	long long  n;
+	cin>>n;
+	while(n!=1){
+	    cout<<n<<" ";
+	    if(n%2==0){
+	        n=n/2;
+	    }
+	    else{
+	        n= (n*3) + 1;
+	    }
+	}
+	cout<<n<<endl;
+ 
 }
